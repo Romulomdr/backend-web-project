60d43a1e9cb79d28c1575e0f6a47019be19f31a1
romulomdr <romulomatheus31@gmail.com>
2023-11-09 20:33:30 -0300

    added jest for tests

--- build/server/routes/index.js
+++ build/server/routes/index.js
@@ -2,13 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
-const http_status_codes_1 = require("http-status-codes");
+const controllers_1 = require("./../controllers");
 const router = (0, express_1.Router)();
 exports.router = router;
 router.get('/teste', (_, res) => {
     return res.send('Ola, DEV!');
 });
-router.post('/teste2', (req, res) => {
-    console.log(req.body);
-    return res.status(http_status_codes_1.StatusCodes.OK).json(req.body);
-});
+router.post('/cidades', controllers_1.CidadesController.createValidation, controllers_1.CidadesController.create);
+router.get('/cidades', controllers_1.CidadesController.getAllValidation, controllers_1.CidadesController.getAll);
+// Para fazer com que ele use o parametro da query o caminho tem que ser diferente
+router.get('/cidades/:id', controllers_1.CidadesController.getByIdValidation, controllers_1.CidadesController.getById);
+router.put('/cidades/:id', controllers_1.CidadesController.updateByIdValidation, controllers_1.CidadesController.updateById);
+router.delete('/cidades/:id', controllers_1.CidadesController.deleteByIdValidation, controllers_1.CidadesController.deleteById);
+//QUANDO VOLTAR FAZER O DELETE
--- build/server/server.js
+++ build/server/server.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.server = void 0;
+require("./shared/services/TranslationsYup");
 const express_1 = __importDefault(require("express"));
 require("dotenv/config");
 const routes_1 = require("./routes");
